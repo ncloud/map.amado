54c90e8f8a330a4931b47a191f864d73612adb96
NCLOUD <me@ncloud.kr>
2013-11-21 20:43:59 +0900

    13.11.21

--- application/views/layouts/map.php
+++ application/views/layouts/map.php
@@ -31,7 +31,7 @@
 	<script type="text/javascript" src="<?php echo site_url("/bootstrap/js/bootstrap-modal.js");?>"></script>
     <script type="text/javascript" src="<?php echo site_url("/bootstrap/js/bootstrap-notify.js");?>"></script>
 
-	<script type="text/javascript" src="<?php echo site_url("/js/basic.js");?>"></script>
+	<script type="text/javascript" src="<?php echo site_url("/js/lib/basic.js");?>"></script>
 
 	<script type="text/javascript">
 		var service = {
